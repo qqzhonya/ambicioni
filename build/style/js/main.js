046d08f44f6cac2c527966872fca0a34733b915d
Eugene Rudenko <egnrdnk@gmail.com>
2020-07-09 18:16:13 +0300

    Initial commit

---
+++ build/style/js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+	
+	//
+	// Header slider
+	//
+	
+	$('.header-slider').owlCarousel({
+		items: 1,
+		loop:true,
+    margin:10,
+		nav:true,
+		dots:true,
+		autoplay:true,
+    autoplayTimeout:2500,
+		autoplayHoverPause:true,
+		animateOut: 'fadeOut'
+	});
+
+	//
+	// Header slider end
+	//
+
+});
